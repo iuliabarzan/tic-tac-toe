64c0bd8742a092634b5706d89e93d1581261d718
Iulia Barzan <iulia_barzan@yahoo.com>
2018-12-05 14:34:26 +0200

    Fix data validation

--- src/components/Players/index.jsx
+++ src/components/Players/index.jsx
@@ -28,31 +28,30 @@ class Players extends React.Component {
   handleClick = () => {
     const { player1, player2 } = this.state;
 
-    player1.trim().length() > 0 && 
-    player2.trim().length() > 0 &&
+    if (player1.length > 0 && player2.length > 0) {
+      this.setState({
+        clicked: true
+      });
+    }
 
-    this.setState({
-      clicked: true
-    });
   }
 
   render () {
-
     return this.state.clicked 
       ? <Game player1 = {this.state.player1} player2 = {this.state.player2}/>
       : (
         <form>
           
-          <label for="p1">Player 1:</label>
+          <label htmlFor="p1">Player 1:</label>
           <input type="text" name="player1" id="p1" onChange={this.handleFirstPlayerChange}/>
           
-          <label for="p2">Player 2:</label>
+          <label htmlFor="p2">Player 2:</label>
           <input type="text" name="player2" id="p2" onChange={this.handleSecondPlayerChange}/>
           
           <button type="submit" value="Start game" onClick={this.handleClick}>
             Start game
           </button>
-          
+
         </form>
       );
   }
